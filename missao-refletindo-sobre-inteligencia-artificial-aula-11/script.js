d9ebedf660d440619d5607f765c344f254a5aa17
jocoski <132354583+jocoski@users.noreply.github.com>
2024-09-06 22:20:50 -0300

    Add files via upload

--- missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
+++ missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com um novo vírus, e você com base em séries e filmes tem uma certa experiência, sim, experiência com zumbis.",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é preocupante...",
+                afirmacao: "No início ficou com medo do que esse vírus pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Isso é incrível!",
+                afirmacao: "Estava empolgado pois cada vez que assistiu ou ouviu uma história sobre zumbis, você já se imaginou em um mundo cheio deles."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "você se reúne com outras pessoas, umas bem experiêntes na ocasião, outras nem tanto. Assim como nos filmes, séries, história que você via, precisa ter uma pessoa para ser o líder, que atitude você toma?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "você se disponibiliza ser o líder, alegando ser experiênte e perito em sobrevivencia.",
+                afirmacao: "Você foi chamado para ser líder!"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Você vota em outra pessoa para ser líder, e avisa que sua vida e a de seus companheiros, estarão nas mãos do líder",
+                afirmacao: "Outra pessoa foi escolhida líder, mas você não ficou seguro com isso."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após a escolha do novo líder, foi orientado por ele vocês se separarem para procurar um abrigo, ir atrás de comida e procurar armamentos, como você se posiciona? ",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "você ajuda o grupo.",
+                afirmacao: "Zumbis acabam aparecendo no seu caminho."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "você não ajuda seu grupo.",
+                afirmacao: "Uma pessoa de seu grupo volta correndo avizando que o resto foi incurralado por zumbis."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como você enfrenta os zumbis? Sozinho? Em grupo? o que vai fazer?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
+                texto: "Cria um plano com seu grupo, acabando com os zumbis sem nenhuma baixa em seu grupo",
+                afirmacao: "Você volta para casa em segurança depois da luta bem sucedida com os zumbis."
+            },   
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Corre para alcançar seu grupo, se angustiando por não ter ido junto.",
+                afirmacao: "Você não chega a tempo e então uma pessoa do seu grupo é mordida e se transforma, então sobra pra você tomar atitude e matar o zumbi, que um dia foi seu amigo..."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "anos se passaram e essa imagem ainda perturba sua mente, você está pensando seriamente em ir embora, se afastar do grupo, com medo de que a mesma coisa aconteça, e que você tenha que tomar a mesma atitude.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "você decide ir embora sem dar satisfação, sem avisar nimguém, apenas desaparece de tudo, e da vida de todos",
+                afirmacao: "nesse tempo andando, tantos dias, tantos quilômetros, você encontra um refúgio de todas as pessoas que sobreviveram,então você pode viver em paz. Mas antes de viver em paz você voltou e resgatou seus outros companheiros, companheiros o qual você se arrependeu de ter deixado-os para trás."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Você vai embora, e se despede de todos seus amigos.",
+                afirmacao: "Você encontrou salvaçâo, parabéns, porém está sozinho..."
             }
         ]
     },
